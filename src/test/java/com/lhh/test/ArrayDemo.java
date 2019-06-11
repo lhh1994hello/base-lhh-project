7931accc480d2d2fe12be9e7b3742dd43b9ea509
lhh <1497392219@qq.com>
2019-06-11 23:21:22 +0800

    1

---
+++ src/test/java/com/lhh/test/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.lhh.test;
+
+import org.junit.Test;
+
+/**
+ * @Author: lhh
+ * @Date: 2019/6/11 20:53
+ * @Version 1.0
+ * 定义数组的几种方式
+ */
+public class ArrayDemo {
+    @Test
+    public void fun1() {
+        int[] array1 = new int[3];
+        int[] array2 = new int[]{1, 2, 3};
+        int[] array3 = {1, 2, 3};
+
+    }
+}
