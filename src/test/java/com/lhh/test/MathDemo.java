cb62540e115f344304e742289ee7b8fdd80982bb
honghui_li <honghui_li@comwave.com.cn>
2019-06-11 18:12:59 +0800

    测试Mathl.random方法

---
+++ src/test/java/com/lhh/test/MathDemo.java
@@ -0,0 +1,17 @@
+package com.lhh.test;
+
+import org.junit.Test;
+
+/**
+ * @author lhh
+ * @date 2019/6/11 18:06
+ * @Description:
+ */
+public class MathDemo {
+
+    @Test
+    public void MathTest01() {
+        double v = Math.random() / Math.random();
+        System.out.println("结果:" + v);
+    }
+}
